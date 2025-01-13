5c6c8aaa5620e1f8fc53ad2df07275c423cccdf3
Promise Okechukwu <okechukwupromise638@gmail.com>
2025-01-13 14:23:00 +0100

    configure colors and keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030516",
+        secondary: "#60A6E7",
+        bgsecondary: "#071626"
+      },
+      fontFamily: {
+        legend: ['"lexend"', 'system-ui'],
+      },
+
+      keyframes: {
+        blink: {
+          "0%, 100%": { opacity: 1 },
+          "50%": { opacity: 0 },
+        },
+        slowReveal: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+      },
+      animation: {
+        blink: "blink 1s step-end infinite",
+        "slow-reveal": "slowReveal 2s ease-in-out forwards",
+      },
+    },
+
   },
   plugins: [],
 }
